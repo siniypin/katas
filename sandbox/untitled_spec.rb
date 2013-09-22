392fd6e9da6bb5db3bb2ed33d4bb04feaffb8202
www-data <www-data@rails.(none)>
2013-09-22 14:44:57 +0000

    5

--- sandbox/untitled_spec.rb
+++ sandbox/untitled_spec.rb
@@ -4,11 +4,11 @@ describe "untitled" do
   
   context "when numbers are under 10" do
     it "spells zero for 0" do
-      answer.should == "zero"
+      answer(0).should == "zero"
     end
 
     it "spells one for 1" do
-      answer.should == "one"
+      answer(1).should == "one"
     end
   end  
 
