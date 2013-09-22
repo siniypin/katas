96a573eaec0498262d674bf6099322e539685da3
www-data <www-data@rails.(none)>
2013-09-22 15:16:19 +0000

    20

--- sandbox/untitled.rb
+++ sandbox/untitled.rb
@@ -1,7 +1,8 @@
 module Humanize
   @@grammar = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 
                5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine",
-               11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen"}
+               11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 
+               20 => "twenty", 30 => "thirty", "40" => "forty", 50 => "fifty"}
 
   def self.spell number
     if (number > 15)
