3e38fb704f424fffc5f514e93bdd498c1a978be7
www-data <www-data@rails.(none)>
2013-09-22 14:50:11 +0000

    9

--- sandbox/untitled_spec.rb
+++ sandbox/untitled_spec.rb
@@ -4,15 +4,15 @@ describe "untitled" do
   
   context "when numbers are under 10" do
     it "spells zero for 0" do
-      answer(0).should == "zero"
+      Humanize::spell(0).should == "zero"
     end
 
     it "spells one for 1" do
-      answer(1).should == "one"
+      Humanize::spell(1).should == "one"
     end
 
     it "spells nine for 9" do
-      answer(9).should == "nine"
+      Humanize::spell(9).should == "nine"
     end
   end  
 
