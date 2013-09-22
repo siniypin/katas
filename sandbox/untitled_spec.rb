9b912e4249b7519b0c3c93af8d102394419add57
www-data <www-data@rails.(none)>
2013-09-22 14:43:23 +0000

    2

--- sandbox/untitled_spec.rb
+++ sandbox/untitled_spec.rb
@@ -2,9 +2,9 @@ require './untitled'
 
 describe "untitled" do
   
-  context "when doing sums" do
-    it "multiplies correctly" do
-      answer.should == (6 * 9)
+  context "when numbers are under 10" do
+    it "spells zero for 0" do
+      answer.should == "zero"
     end
   end  
 
