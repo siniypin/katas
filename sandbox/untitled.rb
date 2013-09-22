9eb9131087b083219daa609f5c17e52e6bbab8bf
www-data <www-data@rails.(none)>
2013-09-22 15:25:42 +0000

    27

--- sandbox/untitled.rb
+++ sandbox/untitled.rb
@@ -2,7 +2,7 @@ module Humanize
   @@grammar = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 
                5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine",
                11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 
-               20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty"}
+               20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 80 => "eighty"}
 
   def self.spell number
     return @@grammar[number] unless @@grammar[number].nil?
