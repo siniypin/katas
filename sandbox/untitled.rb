059ef37a36d59081f7d92927d9a48ed5f81d0563
www-data <www-data@rails.(none)>
2013-09-22 14:58:14 +0000

    14

--- sandbox/untitled.rb
+++ sandbox/untitled.rb
@@ -1,7 +1,7 @@
 module Humanize
   @@grammar = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 
                5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine",
-               11 => "eleven"}
+               11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen"}
 
   def self.spell number
     @@grammar[number]
